3cb759954568358116313ad4c2b51969ca863f2a
aadigithub <41446647+aadigithub@users.noreply.github.com>
2021-03-24 07:38:25 +0530

    Add files via upload

---
+++ InfoPage.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InfoPage.aspx.cs" Inherits="sample.InfoPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 160px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="nameLabel" runat="server" Text="Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="nameTextBox" runat="server"  Width="181px"></asp:TextBox>
+        <%--<asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="* Required Field" ForeColor="Red" ControlToValidate="nameTextBox"></asp:RequiredFieldValidator>--%>
+        <p>
+            <asp:Label ID="fnameLabel" runat="server" Text="Father Name "></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="fnameTextBox" runat="server" Width="183px"></asp:TextBox>
+            <%--<asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="* Required Field" ForeColor="Red" ControlToValidate="fnameTextBox"></asp:RequiredFieldValidator>--%>
+        </p>
+        <p>
+&nbsp;<asp:Label ID="addressLabel" runat="server" Text="Address"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="addressTextBox" runat="server" Height="54px" TextMode="MultiLine" Width="191px"></asp:TextBox>
+            <%--<asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="* Required Field" ForeColor="Red" ControlToValidate="addressTextBox"></asp:RequiredFieldValidator>--%>
+        </p>
+        <p class="auto-style1">
+            <asp:Button ID="saveButton" runat="server" OnClick="saveButton_Click" Text="Save/Insert" Width="128px" />
+        &nbsp;&nbsp;
+            <asp:Button ID="updateButton" runat="server"  Text="Update" Width="128px" />
+        </p>
+        <asp:GridView ID="GridView1" runat="server" Width="631px"   BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" Height="166px" >
+            
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <%--<asp:CheckBox ID="CheckBox1" runat="server" AutoPostBack="true" OnCheckedChanged="CheckBox1_CheckedChanged" />--%>
+                    <asp:LinkButton runat="server" ID="lnkView" OnClick="lnkView_Click" >View </asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                
+            </Columns>
+            
+            
+            
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+        </asp:GridView>
+   </form>
+</body>
+</html>
---
+++ InfoPage.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace sample
+{
+   
+    public partial class InfoPage : System.Web.UI.Page
+    {
+        //string cs = ConfigurationManager.ConnectionStrings["DBConnect"].ConnectionString;
+        SqlConnection con = new SqlConnection("Data Source=AADI;Initial Catalog=Practice_h3_infotech;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                loadrecord();
+
+                updateButton.Enabled = false;
+                
+            }
+
+
+        }
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into info values('" + nameTextBox.Text.ToString() + "','" + fnameTextBox.Text.ToString() + "','" + addressTextBox.Text.ToString() + "') ", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            //ScriptManager.RegisterStartupScript(this,this.GetType(),"script","alert('Successfully Inserted');",true);
+            loadrecord();
+
+        }
+        void loadrecord()
+        {
+
+            SqlCommand cmd = new SqlCommand("select * from  info", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        //protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    updateButton.Enabled = true;
+        //    int rowind = ((GridViewRow)(sender as Control).NamingContainer).RowIndex;
+        //    CheckBox cb = (CheckBox)GridView1.Rows[rowind].FindControl("Chk");
+        //    if (cb.Checked)
+        //    {
+        //        nameTextBox.Text = GridView1.Rows[rowind].Cells[1].Text;
+        //        fnameTextBox.Text = GridView1.Rows[rowind].Cells[2].Text;
+        //        addressTextBox.Text = GridView1.Rows[rowind].Cells[3].Text;
+
+        //    }
+        //    else
+        //    {
+        //        nameTextBox.Text = "";
+        //        fnameTextBox.Text = "";
+        //        addressTextBox.Text = "";
+        //    }
+
+        //}
+
+        protected void updateButton_Click(object sender, EventArgs e)
+        {
+            //con.Open();
+
+            //SqlCommand cmd = new SqlCommand("Insert into info values('" + nameTextBox.Text.ToString() + "','" + fnameTextBox.Text.ToString() + "','" + addressTextBox.Text.ToString() + "') ", con);
+            //cmd.ExecuteNonQuery();
+            //con.Close();
+            ////ScriptManager.RegisterStartupScript(this,this.GetType(),"script","alert('Successfully Inserted');",true);
+            //loadrecord();
+
+            con.Open();
+            //SqlCommand cmd = new SqlCommand("sp_updatedata", con);
+            SqlCommand cmd = new SqlCommand("Update info set name= '" + nameTextBox.Text.ToString() + "', fname='" + fnameTextBox.Text.ToString() + "', address='" + addressTextBox.Text.ToString() + "'where id ='" + nameTextBox.Text.ToString() + "' ", con);
+            cmd.ExecuteNonQuery();
+            cmd.CommandType = CommandType.StoredProcedure;
+           
+            cmd.Parameters.AddWithValue("name", nameTextBox.Text);
+            cmd.Parameters.AddWithValue("fname", fnameTextBox.Text);
+            cmd.Parameters.AddWithValue("address", addressTextBox.Text);
+
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            GridView1.EditIndex = -1;
+            loadrecord(); 
+        }
+
+        //protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        //{
+        //    if (e.CommandName == 'Click_view')
+        //    {
+        //        LinkButton lnkView = (LinkButton)e.CommandSource;
+        //        string dealId = lnkView.CommandArgument;
+        //    }
+        //}
+
+        protected void lnkView_Click(object sender, EventArgs e)
+        {
+            updateButton.Enabled = true;
+            GridViewRow grdrow = (GridViewRow)((LinkButton)sender).NamingContainer;
+            string rowNumber = grdrow.Cells[1].Text;
+            string dealId = grdrow.Cells[2].Text;
+            string dealTitle = grdrow.Cells[3].Text;
+
+            nameTextBox.Text = grdrow.Cells[2].Text;
+            fnameTextBox.Text = grdrow.Cells[3].Text;
+            addressTextBox.Text = grdrow.Cells[4].Text;
+
+
+        }
+    }
+}
+
